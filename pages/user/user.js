9c97c731979c6342137912bc334857ddf9c5bcb5
witpinegit <353565416@qq.com>
2019-12-25 14:58:37 +0800

    我的——授权登录

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,18 +1,62 @@
 // pages/user/user.js
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    toubiao: 0, //我的投标数量
+    shoucang: 0, //我的收藏数量
+    userInfo: {}, //用户信息
+    hasUserInfo: false, //是否已有用户信息，默认false
+    canIUse: wx.canIUse('button.open-type.getUserInfo'), //判断小程序的API，回调，参数，组件等是否在当前版本可用
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 如果全局变量中已有了userInfo,把全局的userInfo拿来使用
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) { //如果当前版本兼容小程序的API，回调，参数，组件等
+      //由于getUserInfo是网络请求，可能会在Page.onLoad之后才返回
+      //所以此处加入callback以防止这种情况
+      app.userInfoReadyback = res =>{
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    }else{
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          //res.userInfo就是用户信息
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+    // console.log(this.data.userInfo);
+    // console.log(this.data.userInfo.nickName);
+    // console.log(this.data.userInfo.avatarUrl);
+    // console.log(this.data.userInfo.gender);
+    // console.log(this.data.userInfo.province);
+    // console.log(this.data.userInfo.city);
+    // console.log(this.data.userInfo.country);
   },
 
   /**
